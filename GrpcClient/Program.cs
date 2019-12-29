4d845fccfd24baf13c7fa61a69b4e6a6e4f57583
Jean Lopes <jean.llopes@outlook.com>
2019-12-29 17:16:09 -0300

    Add annotations Add new service

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ////CONFIGURE THE SERVER
+            #region Client Test 1
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var input = new HelloRequest{ Name = "Jean"};
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+
+            #endregion
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerClient = new Customer.CustomerClient(channel);
+
+            var clientrequested = new CustomerLookupModel { UserId = 1 };
+
+            var customerResponse = await customerClient.GetCustomerInfoAsync(clientrequested);
+
+            Console.WriteLine(customerResponse.FirstName);
+            Console.WriteLine(customerResponse.LastName);
+            Console.WriteLine(customerResponse.Age);
+            Console.WriteLine(customerResponse.EmailAddress);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,53 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            var output = new CustomerModel();
+            switch (request.UserId)
+            {
+                default:
+                    output = DefaultCustomerModelResponse;
+                    break;
+            }
+
+            return Task.FromResult(output);
+        }
+
+        public override Task CreateCustomer(
+            CreateCustomerResponse request, 
+            IServerStreamWriter<CustomerModel> responseStream, 
+            ServerCallContext context)
+        {
+
+
+
+            return base.CreateCustomer(request, responseStream, context);
+        }
+
+        private static CustomerModel DefaultCustomerModelResponse =>
+            new CustomerModel
+            {
+                FirstName = "Found Name",
+                LastName = "Found LastName",
+                Age = 10,
+                EmailAddress = "Found Email",
+                IsAlive = true
+            };
+    }
+}
